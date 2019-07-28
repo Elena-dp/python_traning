50648132522783c02078101c59b9344f24ed7f0c
Len <elenadp@yandex.ru>
2019-07-28 14:54:48 +0300

    дз3, работет

---
+++ addrress.py
@@ -0,0 +1,11 @@
+class Addrress:
+
+    def __init__(self, fname, mname, lname, niname, comp, addrr, homtel, mail):
+        self.fname=fname
+        self.mname=mname
+        self.lname=lname
+        self.niname=niname
+        self.comp=comp
+        self.addrr=addrr
+        self.homtel=homtel
+        self.mail=mail
--- test_add_adr.py
+++ test_add_adr.py
@@ -5,7 +5,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
+from addrress import Addrress
 
 class TestAddAdr(unittest.TestCase):
     def setUp(self):
@@ -14,33 +15,63 @@ class TestAddAdr(unittest.TestCase):
 
     def test_add_adr(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/edit.php")
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.add_new_string(wd, Addrress(fname="Le", mname="An", lname="Ka", niname="eldp", comp="DiPi", addrr="Moj", homtel="+77777777777", mail="el@dprint.ru"))
+        self.return_to_home(wd)
+        self.logout(wd)
+
+    def test_add_empty_adr(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.add_new_string(wd, Addrress(fname="", mname="", lname="", niname="", comp="", addrr="", homtel="", mail=""))
+        self.return_to_home(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        # logout
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_home(self, wd):
+        # retun page home
+        wd.find_element_by_link_text("home").click()
+
+    def add_new_string(self, wd, addrress):
+        # add new string
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("Le")
+        wd.find_element_by_name("firstname").send_keys(addrress.fname)
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("An")
+        wd.find_element_by_name("middlename").send_keys(addrress.mname)
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Ka")
+        wd.find_element_by_name("lastname").send_keys(addrress.lname)
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("eldp")
+        wd.find_element_by_name("nickname").send_keys(addrress.niname)
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("DiPi")
+        wd.find_element_by_name("company").send_keys(addrress.comp)
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("Moj")
+        wd.find_element_by_name("address").send_keys(addrress.addrr)
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("+77777777777")
+        wd.find_element_by_name("home").send_keys(addrress.homtel)
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("el@dprint.ru")
-        wd.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]").click()
-        wd.find_element_by_link_text("home").click()
-        wd.find_element_by_link_text("Logout").click()
-    
+        wd.find_element_by_name("email").send_keys(addrress.mail)
+        # submit....
+        wd.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]").click()
+
+    def login(self, wd, username, password):
+        # login
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        # open home page
+        wd.get("http://localhost/addressbook/edit.php")
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
