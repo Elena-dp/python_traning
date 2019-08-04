ddf181f8eade61c4a5ef14f4a9dea3169580c698
Len <elenadp@yandex.ru>
2019-08-04 10:13:59 +0300

    + rest_edit_adr

---
+++ test/test_edit_adr.py
@@ -0,0 +1,8 @@
+from model.addrress import Addrress
+
+
+
+def test_edit_adr(app):
+    app.session.login(username="admin", password="secret")
+    app.addline.edit(Addrress(fname="N", mname="N", lname="N", niname="N", comp="N", addrr="N", homtel="+71239876543", mail=""))
+    app.session.logout()
