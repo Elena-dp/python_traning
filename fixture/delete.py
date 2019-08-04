5d357af3aa2a062b1822d0886dfa7b82fee17dfe
Len <elenadp@yandex.ru>
2019-08-04 14:23:36 +0300

    создан DeleteHelper для удаления первого адр и первой группы (возможно лишний раз употреблен app или не понимаю его использования)

--- fixture/delete.py
+++ fixture/delete.py
@@ -8,3 +8,12 @@ class DeleteHelper:
         app.select.select_first_adr()
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to_alert().accept()
+
+    def delete_first_group(self,app):
+        wd = self.app.wd
+        app.group.open_groups_page()
+        #select first group
+        app.select.select_first_adr()
+        #submit deletion
+        wd.find_element_by_name("delete").click()
+        app.group.return_to_groups_page()
