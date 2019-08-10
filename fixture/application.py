4624ebfca19f668816c419174f3e172244e3d80b
Len <elenadp@yandex.ru>
2019-08-10 21:53:05 +0300

    проверка валидности фикстуры и проверка состояния сессии

--- fixture/application.py
+++ fixture/application.py
@@ -7,7 +7,7 @@ class Application:
 
     def __init__(self):
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+        self.wd.implicitly_wait(5) # ранее стояло 30
         self.session=SessionHelper (self)
         self.group=GroupHelper(self)
         self.addline=AddlineHelper(self)
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,7 +4,8 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.group.create(Group(name="V", header="V", footer="V"))
+    app.group.create(Group(name="bbb", header="bbrr", footer="bbb"))
+
 
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
