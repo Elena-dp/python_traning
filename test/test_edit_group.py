b1dedcca62ff055574bd9ee9ceca8071508fb51c
Len <elenadp@yandex.ru>
2019-08-10 17:44:14 +0300

    Modify test добавлены, для модификации отдельных полей group.

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -5,3 +5,13 @@ def test_edit_group(app):
     app.session.login(username="admin", password="secret")
     app.group.edit_group(Group(name="3", header="3", footer="3"))
     app.session.logout()
+
+def test_modify_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(name="new group"))
+    app.session.logout()
+
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="new header"))
+    app.session.logout()
