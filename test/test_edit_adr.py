aab1745fd136ab0e2b76f7d454f220111d225f5f
Len <elenadp@yandex.ru>
2019-08-11 21:05:03 +0300

    добавлена проверка в тест edit_adr наличия хотя бы одного адреса, если нет адреса, создается адрес

--- test/test_edit_adr.py
+++ test/test_edit_adr.py
@@ -3,4 +3,6 @@ from model.addrress import Addrress
 
 
 def test_edit_adr(app):
+    if app.addline.count()==0:
+        app.addline.add_new(Addrress(fname="test7", mname="test7", lname="test7", niname="test7", comp="test7", addrr="test7", homtel="+7777444test7", mail="7ehgsl@test"))
     app.addline.edit_adr(Addrress(fname="N", mname="N", lname="N", niname="N", comp="N", addrr="N", homtel="+71239876543", mail=""))
