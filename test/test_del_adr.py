61138961c8947bc090c4669c297b9eb3a2003474
Len <elenadp@yandex.ru>
2019-08-11 12:38:36 +0300

    добавлена таже проверка во второй тест наличия хотя бы одной строки адреса в test_del_adr, если адреса нет, создается 2 адреса  (тест для удаления всех строк)

--- test/test_del_adr.py
+++ test/test_del_adr.py
@@ -7,4 +7,7 @@ def test_del_first_adr(app):
     app.addline.del_first_adr()
 
 def test_del_alladr(app):
+    if app.addline.count() == 0:
+        app.addline.add_new(Addrress(fname="test", mname="test", lname="test", niname="test", comp="test", addrr="test", homtel="+7777444test", mail="ehgsl@test"))
+        app.addline.add_new(Addrress(fname="test 2", mname="test 2", lname="test 2", niname="test 2", comp="test 2", addrr="test 2", homtel="+7777444test", mail="ehgsl@test 2"))
     app.addline.delete_alladr()
