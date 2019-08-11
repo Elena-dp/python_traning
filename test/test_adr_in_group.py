00989cf98f3ebf1f82fa04494dfe6d504ab32ae5
Len <elenadp@yandex.ru>
2019-08-11 12:57:34 +0300

    добавлена проверка в тест наличия хотя бы одной строки адреса и одновременно одной группы в test_adr_in_group, если адреса нет и нет группы, создается адрес и группа

--- test/test_adr_in_group.py
+++ test/test_adr_in_group.py
@@ -1,10 +1,14 @@
-
+from model.addrress import Addrress
+from model.group import Group
 
 def test_adr_in_group(app):
+    if app.addline.count()==0 and app.group.count()==0:
+        app.group.create(Group(name="Test"))
+        app.addline.add_new(Addrress(fname="test 3", mname="test 3", lname="test 3", niname="test 3", comp="test 3", addrr="test 3", homtel="+7777444test3", mail="3ehgsl@test"))
     app.addline.first_adr_fist_group()
 
-def test_adr_in_specgroup(app):
-    app.addline.first_adr_spec_group(app)
+#def test_adr_in_specgroup(app):
+#    app.addline.first_adr_spec_group(app)
 
-def test_alladr_in_specgroup(app):
-    app.addline.alladr_in_specgroup(app)
+#def test_alladr_in_specgroup(app):
+#    app.addline.alladr_in_specgroup(app)
