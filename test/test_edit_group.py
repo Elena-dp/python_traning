310e556f23ed6825fec4e0e66a4357ac5871efb4
Len <elenadp@yandex.ru>
2019-08-11 20:59:19 +0300

    добавлена проверка в тесты edit_group наличия хотя бы одной группы, если нет группы, создается группа

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,10 +2,16 @@ from model.group import Group
 
 
 def test_edit_group(app):
+    if app.group.count()==0:
+        app.group.create(Group(name="test5", header="test5", footer="test5"))
     app.group.edit_group(Group(name="3", header="3", footer="3"))
 
 def test_modify_group_name(app):
+    if app.group.count()==0:
+        app.group.create(Group(name="test6"))
     app.group.modify_first_group(Group(name="new group"))
 
 def test_modify_group_header(app):
+    if app.group.count()==0:
+        app.group.create(Group(header="test7"))
     app.group.modify_first_group(Group(header="new header"))
