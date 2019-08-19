8d8eb5bf4146f6ca6e0517c48cd6a655d0dadd59
Len <elenadp@yandex.ru>
2019-08-19 11:11:13 +0300

    добавлен проверка совпадения длин списков в тесты add_group, edit_group, del_group

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,14 +4,23 @@ from model.group import Group
 def test_edit_group(app):
     if app.group.count()==0:
         app.group.create(Group(name="test5", header="test5", footer="test5"))
+    old_groups=app.group.get_group_list()
     app.group.edit_group(Group(name="3", header="3", footer="3"))
+    new_groups=app.group.get_group_list()
+    assert len(old_groups)==len(new_groups)
 
 def test_modify_group_name(app):
     if app.group.count()==0:
         app.group.create(Group(name="test6"))
+    old_groups=app.group.get_group_list()
     app.group.modify_first_group(Group(name="new group"))
+    new_groups=app.group.get_group_list()
+    assert len(old_groups)==len(new_groups)
 
 def test_modify_group_header(app):
     if app.group.count()==0:
         app.group.create(Group(header="test7"))
+    old_groups=app.group.get_group_list()
     app.group.modify_first_group(Group(header="new header"))
+    new_groups=app.group.get_group_list()
+    assert len(old_groups)==len(new_groups)
