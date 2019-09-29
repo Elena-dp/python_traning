1d74b44c8fe533e755acace6d3fd4f7c0fcaec83
Len <elenadp@yandex.ru>
2019-09-29 20:43:28 +0300

    Этот коммит -ответ на ДЗ 11 (исправления в __eq__) и ответ на ДЗ 12 (Реализовать кеширование при загрузке списков групп и контактов) кеширование и хеширование тестов для удаления и модификации адр

--- test/test_del_adr.py
+++ test/test_del_adr.py
@@ -6,8 +6,8 @@ def test_del_first_adr(app):
         app.addline.add_new(Addrress(fname="test", mname="test", lname="test", niname="test", comp="test", addrr="test", homtel="+7777444test", mail="ehgsl@test"))
     old_list_adr = app.addline.get_adr_list()
     app.addline.del_first_adr()
+    assert len(old_list_adr)-1 == app.addline.count()
     new_list_adr = app.addline.get_adr_list()
-    assert len(old_list_adr)-1 == len(new_list_adr)
     old_list_adr[0:1]=[]
     assert old_list_adr == new_list_adr
 
--- test/test_edit_adr.py
+++ test/test_edit_adr.py
@@ -14,10 +14,10 @@ def test_edit_fname(app):
     if app.addline.count()==0:
         app.addline.add_new(Addrress(fname="test8", mname="tes8", lname="test8", niname="test8", comp="test8", addrr="test8", homtel="+7777444test8", mail="8ehgsl@test"))
     old_list_adr = app.addline.get_adr_list()
-    addrrss = Addrress(fname="New29092019")
+    addrrss = Addrress(fname="New29092019_2")
     addrrss.id=old_list_adr[0].id
     app.addline.edit_adr(addrrss)
+    assert len(old_list_adr) == app.addline.count()
     new_list_adr = app.addline.get_adr_list()
-    assert len(old_list_adr) == len(new_list_adr)
     old_list_adr[0]=addrrss
     assert sorted(old_list_adr, key=Addrress.id_or_max) == sorted(new_list_adr, key=Addrress.id_or_max)
